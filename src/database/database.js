4251d1f1931ef75d5e957c290c756426636a70aa
andresandaar <davilaarroyo07@gmail.com>
2022-02-23 23:32:29 -0500

    make it better12345

--- src/database/database.js
+++ src/database/database.js
@@ -1,31 +1,19 @@
 //conneción con la base de datos
 const mysql = require('mysql');
 
-const mysqlConnection = mysql.createConnection({
+const mysqlConnection = mysql.createPool({
 	host: process.env.DB_HOST || 'localhost',
 	user: process.env.DB_USER || 'root',
 	password: process.env.DB_PASSWORD || 'password',
 	database: process.env.DB_DATABASE || 'crud2',
 });
-/* mysqlConnection.connect((err) => {
+mysqlConnection.connect((err) => {
 	if (err) {
 		console.log(err);
 		return;
 	} else {
 		console.log('Db is connected');
 	}
-}); */
-
-mysqlConnection.connect();
-
-mysqlConnection.query(
-	'SELECT 1 + 1 AS solution',
-	function (error, results, fields) {
-		if (error) throw error;
-		console.log('The solution is: ', results[0].solution);
-	}
-);
-
-mysqlConnection.end();
+});
 
 module.exports = mysqlConnection;
